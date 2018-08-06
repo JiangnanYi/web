fa572e73092a34643d51c286f199a06b5369d73a
JiangnanYi <450983626@qq.com>
2018-08-06 19:09:35 +0800

    yes

---
+++ 0731File/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        String name="1241";
+//        int sun = name.length();
+//        char cr = name.charAt(0);
+            int b=1;
+            String a="";
+
+        for (int i = 1; i <=8; i++) {
+            System.out.print(a+i+"分");
+            a=",";
+        }
+
+    }
+}
